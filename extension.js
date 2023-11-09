f47f5252773b1c052c3c20a42d907a3628db7812
legio <127176580+anonlegionoke@users.noreply.github.com>
2023-11-09 12:03:03 +0530

    Update extension.js

---
+++ extension.js
@@ -0,0 +1,79 @@
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import GLib from 'gi://GLib';
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+const Mainloop = imports.mainloop;
+
+
+let panelButton;
+let panelButtonText;
+
+
+function init() {
+    panelButton = new St.Bin({
+        style_class: "panel-button",
+    });
+
+    panelButtonText = new St.Label({
+        y_align: Clutter.ActorAlign.CENTER,
+    });
+
+    panelButton.set_child(panelButtonText);
+}
+
+function updateGreeting() {
+    const myArray = ["Good Morning 🌅️", "Good Afternoon 🌞", "Good Evening 🌇️", "Night 🌃️"];
+    const now = GLib.DateTime.new_now_local();
+    const nowString = now.format("%H:%M:%S");
+
+let currentIndex = 0;
+
+    if (nowString < "05:00:00") {
+        currentIndex = 3;
+    } else if (nowString < "12:00:00") {
+        currentIndex = 0;
+    } 
+      else if (nowString < "17:00:00"){
+        currentIndex = 1;
+    }
+      else if (nowString < "22:00:00") {
+        currentIndex = 2;
+    }
+      else if (nowString < "23:59:59"){
+        currentIndex = 3;
+    }
+  
+
+    panelButtonText.set_text(myArray[currentIndex]);
+}
+
+export default class greetingsonpanel extends Extension{
+constructor(metadata) {
+  super (metadata);
+  }
+  
+  enable(){
+  
+     init();
+     
+    Main.panel._centerBox.insert_child_at_index(panelButton, -1);
+    let sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
+    updateGreeting(); 
+    const runAgain = true;
+
+    if (runAgain)
+        return GLib.SOURCE_CONTINUE;
+
+    return GLib.SOURCE_REMOVE;
+     });
+  }
+
+  disable() {
+    Main.panel._centerBox.remove_child(panelButton);
+   }
+}
+
+
+
